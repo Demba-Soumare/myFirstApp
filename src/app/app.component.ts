8492bbcd4e2e81819aeff2bd81ed1b93ff49bcd4
demba <demba08hotmail.fr>
2024-05-27 12:21:22 +0200

    component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ValidationButtonComponent } from './validation-button/validation-button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ValidationButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'mBienvenue sur le site de [ton nom] !';
+  title = 'mBienvenue sur le site de Demba!';
 }
